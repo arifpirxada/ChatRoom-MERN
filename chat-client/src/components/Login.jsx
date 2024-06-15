87bdce14e02dd1d62c88c0aeae6e61291a0c9f17
arifpirxada <pirxadaarif@gmail.com>
2024-06-15 21:56:41 +0530

    Update: "wrote some basic tests"

--- chat-client/src/components/Login.jsx
+++ chat-client/src/components/Login.jsx
@@ -106,7 +106,7 @@ function Login({ setLoginStatus, setUserName }) {
                             </svg>
                         </div>
                         Sign up</button>
-                    <span onClick={ toggleRegistration } className="flex text-white gap-2">Already have an account? <p className="text-blue-500 hover:text-blue-300 cursor-pointer">Login</p></span>
+                    <span data-testid="login-toggle" onClick={ toggleRegistration } className="flex text-white gap-2">Already have an account? <p className="text-blue-500 hover:text-blue-300 cursor-pointer">Login</p></span>
                     <span className="text-white bg-orange-500 p-2 rounded transition-all opacity-0">{ signMessage }</span>
                 </form>
                 <form onSubmit={ login } className={ `${registration ? "hidden" : "flex"} transition-all flex-col gap-4 items-center` }>
@@ -118,7 +118,7 @@ function Login({ setLoginStatus, setUserName }) {
                             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                         </svg>
                     </div>Login</button>
-                    <span onClick={ toggleRegistration } className="flex text-white gap-2">Already have an account? <p className="text-blue-500 hover:text-blue-300 cursor-pointer">Sign up</p></span>
+                    <span data-testid="signup-toggle" onClick={ toggleRegistration } className="flex text-white gap-2">Already have an account? <p className="text-blue-500 hover:text-blue-300 cursor-pointer">Sign up</p></span>
                     <span className="text-white bg-orange-500 p-2 rounded transition-all opacity-0">{ logMessage }</span>
                 </form>
             </div>
